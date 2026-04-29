299a2148dd62f9e605ea713fe78ce16c9e1dd4f4
Jaisurya3101 <162279151+Jaisurya3101@users.noreply.github.com>
2026-04-29 20:26:39 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
+        }
+        if(nums.length != set.size()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
