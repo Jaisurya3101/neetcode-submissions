e60d1056ac794afcb7f0868f96df430b31a77108
Jaisurya3101 <162279151+Jaisurya3101@users.noreply.github.com>
2026-09-16 19:07:19 +0530

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int k = n / 3;
+
+        List<Integer> l = new ArrayList<>();
+        Arrays.sort(nums);
+        if (n < 3) {
+            for (int num : nums) {
+                l.add(num);
+            }
+            return l;
+        }
+        for (int i = 0; i + k < n; i++) {
+            if (nums[i] == nums[i + k]) {
+                l.add(nums[i]);
+                while (i + k < n && nums[i] == nums[i + k]) {
+                    i++;
+                }
+            }
+        }
+        return l;
+    }
+}
